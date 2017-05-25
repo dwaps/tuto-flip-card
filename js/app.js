4f0e4a0d3fe7bd0b6cb5afc27cb0ce619c0bcf42
Michael Cornillon <contact@dwaps.fr>
2017-05-25 15:02:11 +0200

    Gestion du clic sur les cartes

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const NB_CARDS = 6;
 var card = document.querySelector(".card");
 var cards = [];
 
+var randomNb = 0;
+
 for(var i = 0; i < NB_CARDS; i++)
 {
   if(i == 0) // Si 1er tour : on ajoute la carte du DOM
@@ -14,4 +16,21 @@ for(var i = 0; i < NB_CARDS; i++)
     cards.push(card.cloneNode(true));
     card.parentNode.appendChild(cards[i]);
   }
+
+  // Gestion du clic sur la carte en cours
+  cards[i].addEventListener(
+    "click",
+    function()
+    {
+      if( !this.className.match("flipped") )
+      {
+        randomNb = parseInt( (Math.random()*20) + 1 );
+        this.lastElementChild.innerHTML = randomNb;
+        this.setAttribute("class", "card flipped");
+      }
+      else
+        this.setAttribute("class", "card");
+    },
+    false
+  );
 }
