539c18ab3c2951941c025d49b161157a7696928d
Michael Cornillon <contact@dwaps.fr>
2017-05-25 15:17:11 +0200

    Génération nb aléatoire pour paire de cartes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const NB_CARDS = 6;
+const NB_CARDS = 6; // Nb pair obligatoirement !!!
 
 var card = document.querySelector(".card");
 var cards = [];
@@ -7,15 +7,19 @@ var randomNb = 0;
 
 for(var i = 0; i < NB_CARDS; i++)
 {
+  // CREATION DES CARTES
   if(i == 0) // Si 1er tour : on ajoute la carte du DOM
   {
+    generateNb(card, i);
     cards.push(card);
   }
   else // Sinon on clone la carte du DOM
   {
     cards.push(card.cloneNode(true));
+    generateNb(cards[i], i);
     card.parentNode.appendChild(cards[i]);
   }
+
 
   // Gestion du clic sur la carte en cours
   cards[i].addEventListener(
@@ -23,14 +27,19 @@ for(var i = 0; i < NB_CARDS; i++)
     function()
     {
       if( !this.className.match("flipped") )
-      {
-        randomNb = parseInt( (Math.random()*20) + 1 );
-        this.lastElementChild.innerHTML = randomNb;
         this.setAttribute("class", "card flipped");
-      }
       else
         this.setAttribute("class", "card");
     },
     false
   );
+}
+
+// Génération des nombres des cartes
+function generateNb(pCard, cpt)
+{
+  if(cpt%2 == 0) // Si le compteur est pair, on peut générer un nouveau nombre
+    randomNb = parseInt( (Math.random()*20) + 1 );
+
+  pCard.lastElementChild.innerHTML = randomNb;
 }
