e0074f78a3ba9c27874b448bafa3b81835eb04fc
Michael Cornillon <contact@dwaps.fr>
2017-05-28 19:42:17 +0200

    2eme optimisation du code (POO)

--- js/app.js
+++ js/app.js
@@ -1,182 +1,204 @@
-const NB_CARDS = 6; // Nb pair obligatoirement !!!
-const HIDDEN_CARD = "card";
-const SHOWED_CARD = "card flipped";
-const FOUND_CARD = "card flipped found";
 
-var wrapper = document.querySelector(".wrapper");
+var dwapsPairsGame = {
+  NB_CARDS: 6, // Nb pair obligatoirement !!!
+  HIDDEN_CARD: "card",
+  SHOWED_CARD: "card flipped",
+  FOUND_CARD: "card flipped found",
 
-var card = createCard();
-var cards = [], firstFlippedCard = null;
+  wrapper: document.querySelector(".wrapper"),
 
-var randomNb = 0, totalFlipped = 0;
-var tabRandomNb = [];
+  card: null,
+  cards: [],
+  firstFlippedCard: null,
 
-var youWon = false;
+  randomNb: 0,
+  totalFlipped: 0,
+  tabRandomNb: [],
 
+  youWon: false,
 
-// CREATION DES CARTES
-for(var i = 0; i < NB_CARDS; i++)
-{
-  if(i == 0) // Si 1er tour : on ajoute la carte du DOM
+  init: function()
   {
-    generateNb(card, i);
-    cards.push(card);
-  }
-  else // Sinon on clone la carte du DOM
-  {
-    cards.push(card.cloneNode(true));
-    generateNb(cards[i], i);
-  }
+    var THIS = this; // Référence à l'objet général dwapsPairsGame
 
+    this.createCard();
 
-  // Gestion du clic sur la carte en cours
-  cards[i].addEventListener(
-    "click",
-    function()
+    // CREATION DES CARTES
+    for(var i = 0; i < this.NB_CARDS; i++)
     {
-      if( this.className != FOUND_CARD ) // Pas besoin d'aller plus loin si la carte est déjà validée
+      if(i == 0) // Si 1er tour : on ajoute la carte du DOM
       {
-        if( this.className != SHOWED_CARD ) // Si la carte n'est pas retournée...
+        this.generateNb(this.card, i);
+        this.cards.push(this.card);
+      }
+      else // Sinon on clone la carte du DOM
+      {
+        this.cards.push(this.card.cloneNode(true));
+        this.generateNb(this.cards[i], i);
+      }
+
+
+      // Gestion du clic sur la carte en cours
+      this.cards[i].addEventListener(
+        "click",
+        function()
         {
-          // ... on la retourne...
-          this.setAttribute("class", SHOWED_CARD);
-
-          // ... et on procède à la comparaison s'il y a déjà une carte retournée
-          if( firstFlippedCard )
+          if( this.className != THIS.FOUND_CARD ) // Pas besoin d'aller plus loin si la carte est déjà validée
           {
-            if( firstFlippedCard.innerHTML == this.innerHTML ) // Si les deux cartes ont le même numéro
+            if( this.className != THIS.SHOWED_CARD ) // Si la carte n'est pas retournée...
             {
-              firstFlippedCard.setAttribute("class", FOUND_CARD);
-              this.setAttribute("class", FOUND_CARD);
+              // ... on la retourne...
+              this.setAttribute("class", THIS.SHOWED_CARD);
 
-              firstFlippedCard = null;
+              // ... et on procède à la comparaison s'il y a déjà une carte retournée
+              if( THIS.firstFlippedCard )
+              {
+                if( THIS.firstFlippedCard.innerHTML == this.innerHTML ) // Si les deux cartes ont le même numéro
+                {
+                  THIS.firstFlippedCard.setAttribute("class", THIS.FOUND_CARD);
+                  this.setAttribute("class", THIS.FOUND_CARD);
+
+                  THIS.firstFlippedCard = null;
+                }
+                else // Sinon on retourne les cartes pour les cacher
+                {
+                  var THAT = this; // Référence à l'objet this.cards[i]
+
+                  setTimeout(
+                    function()
+                    {
+                      THIS.firstFlippedCard.setAttribute("class", THIS.HIDDEN_CARD);
+                      THAT.setAttribute("class", THIS.HIDDEN_CARD);
+
+                      THIS.firstFlippedCard = null;
+                    },
+                    500
+                  );
+                }
+              }
+              else // S'il n'y a pas encore de carte retournée alors celle-ci est la 1ère !
+              {
+                THIS.firstFlippedCard = this;
+              }
             }
-            else // Sinon on retourne les cartes pour les cacher
+            else
             {
-              var THIS = this;
+              this.setAttribute("class", THIS.HIDDEN_CARD);
+            }
+          }
+
+          // On vérifie à chaque fois si le jeu est fini ou non
+          // S'il est fini, on affiche un message, on retourne toutes les cartes,
+          // et on génère de nouvelles paires de cartes
+          if(!THIS.youWon)
+          {
+            for(var i = 0; i < THIS.cards.length; i++)
+            {
+              if(THIS.cards[i].className == THIS.FOUND_CARD)
+                THIS.totalFlipped++;
+            }
+
+            if(THIS.totalFlipped == THIS.NB_CARDS)
+            {
+              THIS.youWon = true;
 
               setTimeout(
                 function()
                 {
-                  firstFlippedCard.setAttribute("class", HIDDEN_CARD);
-                  THIS.setAttribute("class", HIDDEN_CARD);
+                  alert("Bravo !");
 
-                  firstFlippedCard = null;
+                  // Réinitialisation du jeu
+                  window.location.reload();
                 },
                 500
               );
+
             }
-          }
-          else // S'il n'y a pas encore de carte retournée alors celle-ci est la 1ère !
-          {
-            firstFlippedCard = this;
-          }
-        }
-        else
-        {
-          this.setAttribute("class", HIDDEN_CARD);
-        }
-      }
 
-      // On vérifie à chaque fois si le jeu est fini ou non
-      // S'il est fini, on affiche un message, on retourne toutes les cartes,
-      // et on génère de nouvelles paires de cartes
-      if(!youWon)
+            THIS.totalFlipped = 0;
+          }
+        },
+        false
+      );
+    }
+  },
+
+  createCard: function()
+  {
+    this.card = document.createElement('div');
+      this.card.className = "card";
+
+    var backCard = document.createElement('div');
+      backCard.className = "back";
+    var frontCard = document.createElement('div');
+      frontCard.className = "front";
+
+      this.card.appendChild(backCard);
+      this.card.appendChild(frontCard);
+  },
+
+  // Génération des nombres des cartes
+  generateNb: function(pCard, cpt)
+  {
+    if(cpt%2 == 0) // Si le compteur est pair, on peut générer un nouveau nombre
+    {
+      this.randomNb = parseInt( (Math.random()*20) + 1 );
+
+      // On s'assure de l'unicité des paires
+      // --> Tant que le nombre a déjà été utilisé, on en génère un nouveau
+      while( this.tabRandomNb.length > 0
+              &&
+              this.tabRandomNb.indexOf(this.randomNb) > -1 )
       {
-        for(var i = 0; i < cards.length; i++)
-        {
-          if(cards[i].className == FOUND_CARD)
-            totalFlipped++;
-        }
-
-        if(totalFlipped == NB_CARDS)
-        {
-          youWon = true;
-
-          setTimeout(
-            function()
-            {
-              alert("Bravo !");
-
-              // Réinitialisation du jeu
-              window.location.reload();
-            },
-            500
-          );
-
-        }
-
-        totalFlipped = 0;
+        this.randomNb = parseInt( (Math.random()*20) + 1 );
       }
-    },
-    false
-  );
-}
 
+      this.tabRandomNb.push(this.randomNb);
+    }
 
-// Mélange des cartes et affichage
-shakeCards();
-for(var i = 0; i < cards.length; i++)
-  wrapper.appendChild(cards[i]);
+    pCard.lastElementChild.innerHTML = this.randomNb;
+  },
 
-
-
-// FONCTIONS
-
-// Création d'une carte
-function createCard()
-{
-  var card = document.createElement('div');
-    card.className = "card";
-
-  backCard = document.createElement('div');
-    backCard.className = "back";
-  frontCard = document.createElement('div');
-    frontCard.className = "front";
-
-    card.appendChild(backCard);
-    card.appendChild(frontCard);
-
-  return card;
-}
-
-// Génération des nombres des cartes
-function generateNb(pCard, cpt)
-{
-  if(cpt%2 == 0) // Si le compteur est pair, on peut générer un nouveau nombre
+  // Mélange des cartes
+  shakeCards: function()
   {
-    randomNb = parseInt( (Math.random()*20) + 1 );
+    // Parcours de tout le tableau des cartes (depuis la fin) sauf la première case
+    // Celle-ci n'a pas besoin d'être traitée...
+    for(var i = this.cards.length-1; i > 0; i--)
+    {
+      // Génération d'un index aléatoire
+      var randomIndex = Math.floor( Math.random() * (i+1) );
+      
+      // On sauvegarde la valeur se trouvant à cet index aléatoire pour ne pas la perdre
+      var valAtIndex = this.cards[randomIndex];
 
-    // On s'assure de l'unicité des paires
-    // --> Tant que le nombre a déjà été utilisé, on en génère un nouveau
-    while( tabRandomNb.length > 0 && tabRandomNb.indexOf(randomNb) > -1 )
-      randomNb = parseInt( (Math.random()*20) + 1 );
+      // On peut maintenant écraser (remplacer) cette valeur par celle de l'index courant
+      this.cards[randomIndex] = this.cards[i];
 
-    tabRandomNb.push(randomNb);
-  }
+      // Puisqu'on a déplacé la valeur de l'index courant,
+      // on remplace cette dernière par celle qu'on avait sauvegardée plus haut.
+      // Au final, on a inversé les 2 valeurs concernées
+      this.cards[i] = valAtIndex;
+    }
+  },
 
-  pCard.lastElementChild.innerHTML = randomNb;
-}
-
-// Mélange des cartes
-function shakeCards()
-{
-  // Parcours de tout le tableau des cartes (depuis la fin) sauf la première case
-  // Celle-ci n'a pas besoin d'être traitée...
-  for(var i = cards.length-1; i > 0; i--)
+  displayCards: function()
   {
-    // Génération d'un index aléatoire
-    var randomIndex = Math.floor( Math.random() * (i+1) );
-    
-    // On sauvegarde la valeur se trouvant à cet index aléatoire pour ne pas la perdre
-    var valAtIndex = cards[randomIndex];
+    var THIS = this;
 
-    // On peut maintenant écraser (remplacer) cette valeur par celle de l'index courant
-    cards[randomIndex] = cards[i];
+    this.cards.forEach(
+      function(card)
+      {
+        THIS.wrapper.appendChild(card);
+      }
+    );
+  },
 
-    // Puisqu'on a déplacé la valeur de l'index courant,
-    // on remplace cette dernière par celle qu'on avait sauvegardée plus haut.
-    // Au final, on a inversé les 2 valeurs concernées
-    cards[i] = valAtIndex;
+  start: function()
+  {
+    this.init();
+    this.shakeCards();
+    this.displayCards();
   }
-}
+};
+
