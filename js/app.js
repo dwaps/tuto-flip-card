500b017878f76d628a50c949182a2e4b3dc95df3
Michael CORNILLON <contact@dwaps.fr>
2019-07-07 19:54:46 +0200

    Objet littéral --> module JS

--- js/app.js
+++ js/app.js
@@ -1,37 +1,33 @@
 
-const dwapsPairsGame = {
-  NB_CARDS: prompt('Choisissez un nombre de cartes\n(nombre pair) :'), // Nb pair obligatoirement !!!
-  wrapper: document.querySelector('.wrapper'),
-  cards: [],
-  firstFlippedCard: null,
-  totalFlipped: 0,
-  tabRandomNb: [], // Permet d'éviter les doublons
-  youWonSound: null,
+const DwapsPairsGame = (function(nbCards) {
 
-  run() {
-    this.init();
-    this.shuffleCards();
-    this.displayCards();
-  },
+  // DECLARATIONS
+  this.NB_CARDS = nbCards;
+  this.wrapper = document.querySelector('.wrapper');
+  this.cards = [];
+  this.firstFlippedCard = null;
+  this.totalFlipped = 0;
+  this.tabRandomNb = []; // Permet d'éviter les doublons
+  this.youWonSound = null;
 
-  init() {
+  function init() {
+    loadSound();
     
     // Correction incohérence de l'utilisateur
-    if (this.NB_CARDS !== '' && +this.NB_CARDS > 0) {
+    if (this.NB_CARDS !== '' && +this.NB_CARDS > 0 && +this.NB_CARDS <= 20) {
       this.NB_CARDS = (this.NB_CARDS % 2 === 0) ? this.NB_CARDS : +this.NB_CARDS + 1;
       
       const card = createCard();
-      this.initSound();
 
       // CREATION DES CARTES
       for (var i = 0; i < this.NB_CARDS; i++) {
         if (i == 0) {
           this.cards.push(card);
-          this.generateNb(card, i);
+            generateNb(card, i);
         }
         else { // Sinon on clone la carte du DOM
           this.cards.push(card.cloneNode(true));
-          this.generateNb(this.cards[i], i);
+            generateNb(this.cards[i], i);
         }
 
         // Gestion du clic sur la carte en cours
@@ -71,33 +67,40 @@ const dwapsPairsGame = {
               this.classList.add('card');
             }
 
-            this.checkIfGameEnds();
+            checkIfGameEnds();
           }
         );
-      }
-
-      // FUNCTIONS
-      function createCard() {
-        const card = document.createElement('div');
-        const backCard = document.createElement('div');
-        const frontCard = document.createElement('div');
-
-        card.className = 'card';
-        backCard.className = 'back';
-        frontCard.className = 'front';
-
-        card.appendChild(backCard);
-        card.appendChild(frontCard);
-
-        return card;
       }
     }
     else {
       document.write('Pas envie de jouer ? Tans pis... 😢');
     }
-  },
+  }
 
-  initSound() {
+  run();
+
+  function run() {
+    init();
+    shuffleCards();
+    displayCards();
+  }
+
+  function createCard() {
+    const card = document.createElement('div');
+    const backCard = document.createElement('div');
+    const frontCard = document.createElement('div');
+
+    card.className = 'card';
+    backCard.className = 'back';
+    frontCard.className = 'front';
+
+    card.appendChild(backCard);
+    card.appendChild(frontCard);
+
+    return card;
+  }
+
+  function loadSound() {
     this.youWonSound = document.createElement('audio');
     const backSound = document.createElement('audio');
 
@@ -110,12 +113,12 @@ const dwapsPairsGame = {
 
     document.body.appendChild(this.youWonSound);
     document.body.appendChild(backSound);
-  },
+  }
 
-  generateNb(card, index) {
-    if (index%2 == 0) {
+  function generateNb(card, index) {
+    if (index % 2 == 0) {
       do {
-        this.randomNb = Math.ceil((Math.random()*20));
+        this.randomNb = Math.ceil((Math.random() * 20));
       }
       while (this.tabRandomNb.includes(this.randomNb));
 
@@ -123,20 +126,19 @@ const dwapsPairsGame = {
     }
 
     card.lastElementChild.textContent = this.randomNb;
-  },
+  }
 
-  shuffleCards() {
-    this.cards.sort(() => Math.random()-0.5);
-  },
+  function shuffleCards() {
+    this.cards.sort(() => Math.random() - 0.5);
+  }
 
-  displayCards() {
+  function displayCards() {
     this.cards.forEach(card => {
-        this.wrapper.appendChild(card);
-      }
-    );
-  },
-  
-  checkIfGameEnds() {
+      this.wrapper.appendChild(card);
+    });
+  }
+
+  function checkIfGameEnds() {
     for (let card of this.cards) {
       if (card.classList.contains('found')) {
         this.totalFlipped++;
@@ -158,4 +160,5 @@ const dwapsPairsGame = {
     }
   }
 
-};
+  return { init, run, shuffleCards, displayCards };
+});
